ec21d463c5f5f48b1222f99263a718e00066e6e2
sofee <sofee@sofees-MacBook-Air.local>
2017-08-29 12:04:52 +0300

    fixes

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -7,9 +7,9 @@ class MenuList extends Component {
   render() {
     return (
       <Menu secondary>
-        <Menu.Item href='/'>Добавить</Menu.Item>
-        <Menu.Item href='/video'>К просмотру</Menu.Item>
-        <Menu.Item href='/learn'>К изучению</Menu.Item>
+        <Menu.Item href='/'>Add links</Menu.Item>
+        <Menu.Item href='/video'>To Watch</Menu.Item>
+        <Menu.Item href='/learn'>To Learn</Menu.Item>
       </Menu>
     )
   }
